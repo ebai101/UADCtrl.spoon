e07d76a780f925d9c32468860e463fa65a98410c
Ethan Bailey <ebailey256@gmail.com>
2024-05-18 11:34:12 -0400

    python scripts

---
+++ sniffer.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+from scapy.all import *
+from threading import Thread
+from pprint import pprint
+import socket
+import time
+import json
+
+
+class Sniffer(Thread):
+    def __init__(self, filter=True):
+        super().__init__()
+        self.stop_sniffer = Event()
+        self.filter = filter
+
+    def uad_prn(self, pkt):
+        if pkt.haslayer(Raw) and pkt.haslayer(TCP):
+            if pkt[TCP].sport == 4710 or pkt[TCP].dport == 4710:
+                try:
+                    data = pkt[Raw].load.decode().replace("\u0000", "")
+                except:
+                    pass
+                else:
+                    if not filter or not (
+                        "ClockLocked" in data
+                        or "MeterPulse" in data
+                        or "/ping" in data
+                        or "/UndoRedo" in data
+                        or "/UndoRecording" in data
+                    ):
+                        try:
+                            data_json = json.loads(data)
+                            print(
+                                f'{time.asctime(time.localtime(pkt.time))} {"SEND" if pkt[TCP].dport == 4710 else "RECV"}'
+                            )
+                            pprint(data_json)
+                        except:
+                            print(
+                                f'{time.asctime(time.localtime(pkt.time))} {"SEND" if pkt[TCP].dport == 4710 else "RECV"} {data}'
+                            )
+
+    def run(self):
+        sniff(iface="lo0", prn=self.uad_prn, stop_filter=self.should_stop_sniffer)
+
+    def join(self, timeout=None):
+        self.stop_sniffer.set()
+        super().join(timeout)
+
+    def should_stop_sniffer(self, packet):
+        return self.stop_sniffer.is_set()
+
+
+if __name__ == "__main__":
+    sniffer = Sniffer(filter=True)
+    sniffer.start()
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("127.0.0.1", 4710))
+
+    try:
+        while True:
+            message = input()
+            s.send(f"{message}\0".encode("utf-8"))
+    except KeyboardInterrupt:
+        s.close()
+        sniffer.join(2.0)
+        if sniffer.is_alive():
+            sniffer.socket.close()
---
+++ volume.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import socket
+import json
+import sys
+import pprint
+
+quant_factor = 54
+
+
+def monitorLevelQuantized():
+    s.send(b"get /devices/0/outputs/4/CRMonitorLevelTapered/value\0")
+    monitorLevel = json.loads(recvall(s).decode().replace("\u0000", ""))["data"]
+    print(monitorLevel * quant_factor)
+    return round(monitorLevel * quant_factor) / quant_factor
+
+
+def recvall(sock):
+    data = bytearray()
+    while True:
+        packet = sock.recv(4096)
+        if not packet:
+            break
+        data.extend(packet)
+        if b"\x00" in data:
+            break
+    return data
+
+
+if __name__ == "__main__":
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("127.0.0.1", 4710))
+
+    try:
+        command = sys.argv[1]
+    except:
+        print("need command")
+        sys.exit(1)
+
+    s.send(b"set /Sleep false\0")
+
+    match command:
+        case "up":
+            mlq = monitorLevelQuantized()
+            newMonitorLevel = mlq + (1 / quant_factor) if mlq < 1.0 else 1.0
+            s.send(
+                f"set /devices/0/outputs/4/CRMonitorLevelTapered/value {newMonitorLevel}\0".encode()
+            )
+            print("volume up")
+            print(f"new volume is {newMonitorLevel}")
+        case "down":
+            mlq = monitorLevelQuantized()
+            newMonitorLevel = mlq - (1 / quant_factor) if mlq > 0.0 else 0.0
+            s.send(
+                f"set /devices/0/outputs/4/CRMonitorLevelTapered/value {newMonitorLevel}\0".encode()
+            )
+            print("volume down")
+            print(f"new volume is {newMonitorLevel}")
+        case "mute":
+            s.send(b"get /devices/0/outputs/4/Mute/value\0")
+            mute = json.loads(recvall(s).decode().replace("\u0000", ""))["data"]
+            mute = 1 if not mute else 0
+            s.send(f"set /devices/0/outputs/4/Mute/value {mute}\0".encode())
+            print("muted" if mute == 1 else "unmuted")
+        case _:
+            print("invalid command")
+
+    s.close()
